a1d529f9d34d872ceb4abcad292d227f4a12d818
ilaak-03 <Ilaa@meyicloud.com>
2026-01-28 14:47:58 +0530

    Updated source code

--- src/devops/codepipeline/deploy-resources.sh
+++ src/devops/codepipeline/deploy-resources.sh
@@ -1,12 +1,17 @@
+# ⚠️ Local-only helper. CI/CD deploys this stack in real environments.
+
 #!/bin/bash
 set -e
 
 ENV=$1
 source ../env/${ENV}-env.sh
 
-aws cloudformation deploy \
-  --template-file resources.yaml \
+sam build -t resources.yaml
+
+sam deploy \
+  --template-file .aws-sam/build/template.yaml \
   --stack-name file-flow-resources-${ENV} \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-      Environment=$ENV
+  --region ap-south-1 \
+  --no-confirm-changeset \
+  --no-fail-on-empty-changeset
--- src/devops/codepipeline/deploy.sh
+++ src/devops/codepipeline/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
+# First argument: environment
 ENV=$1
+
+# Load environment variables
 source ../env/${ENV}-env.sh
 
+# Deploy the CodePipeline stack
 aws cloudformation deploy \
   --template-file pipeline.yaml \
   --stack-name file-flow-pipeline-${ENV} \
@@ -15,4 +19,4 @@ aws cloudformation deploy \
       CodeStarConnectionArn=$CODESTAR_ARN \
       ArtifactBucketName=$ARTIFACT_BUCKET \
       Environment=$ENV \
-      AWSAccountId=$AWS_ACCOUNT_ID 
+      AWSAccountId=$AWS_ACCOUNT_ID
